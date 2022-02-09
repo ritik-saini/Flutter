1c7619238957205fec793000123ea14df2e64092
Ritik Saini <ritiksaini351@gmail.com>
2022-02-09 16:22:13 +0530

    Delete demo_second directory

--- demo_second/android/app/src/main/java/com/flutterapp/demo_second/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.flutterapp.demo_second;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- demo_second/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import './Classes/home.dart';
-void main(){
-  runApp(MaterialApp(
-    title: "Second Demo App",
-    debugShowCheckedModeBanner: false,
-    home: Home(),
-  ));
-}
